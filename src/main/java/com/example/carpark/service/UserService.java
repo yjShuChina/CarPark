6cccb3cae1222c415a8f65e6b1cdc5bfd3ae4f18
guodaxia_global <3070544317@qq.com>
2020-04-23 09:34:30 +0800

    4.23--郭子淳提交--修改bug

---
+++ src/main/java/com/example/carpark/service/UserService.java
@@ -0,0 +1,5 @@
+package com.example.carpark.service;
+
+public interface UserService {
+
+}
---
+++ src/main/java/com/example/carpark/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.carpark.service.impl;
+
+import com.example.carpark.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
