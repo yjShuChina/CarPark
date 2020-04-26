e95f0bdc936c685f1bb83f676c19e507016f9710
ltx0414 <229573368@qq.com>
2020-04-26 12:16:40 +0800

    4.26 林堂星收费员修改完成

--- src/main/java/com/example/carpark/controller/AdminController.java
+++ src/main/java/com/example/carpark/controller/AdminController.java
@@ -612,10 +612,6 @@ public class AdminController {
     {
         int pageInt = Integer.valueOf(page);
         int limitInt = Integer.valueOf(limit);
-//        int forbidden = adminService.forbiddenState(stateId);
-        int open = adminService.openState(oId);
-        int resign = adminService.resignState(resignId);
-	    int rest = adminService.resetPwd(resetId);
         List<TbCashier> list = adminService.findAll(uid,pageInt,limitInt,startTime,endTime);
         int count = adminService.findCount(uid,startTime,endTime);
         DataManagementResult dataManagementResult = new DataManagementResult();
@@ -628,10 +624,70 @@ public class AdminController {
 
     @RequestMapping("/cashierForbidden")
     @ResponseBody
-    public void cashierForbidden(String page, String limit, String uid, String startTime, String endTime, String stateId, String oId,String resignId,String resetId)
+    public DataManagementResult cashierForbidden(String page, String limit, String uid, String startTime, String endTime, String stateId)
     {
+        int pageInt = Integer.valueOf(page);
+        int limitInt = Integer.valueOf(limit);
         int forbidden = adminService.forbiddenState(stateId);
+        List<TbCashier> list = adminService.findAll(uid,pageInt,limitInt,startTime,endTime);
+        int count = adminService.findCount(uid,startTime,endTime);
+        DataManagementResult dataManagementResult = new DataManagementResult();
+        dataManagementResult.setCode(0);
+        dataManagementResult.setMsg("");
+        dataManagementResult.setCount(count);
+        dataManagementResult.setData(list);
+        return dataManagementResult;
     }
+	@RequestMapping("/cashierOpen")
+	@ResponseBody
+	public DataManagementResult cashierOpen(String page, String limit, String uid, String startTime, String endTime,String oId)
+	{
+		int pageInt = Integer.valueOf(page);
+		int limitInt = Integer.valueOf(limit);
+		int open = adminService.openState(oId);
+		List<TbCashier> list = adminService.findAll(uid,pageInt,limitInt,startTime,endTime);
+		int count = adminService.findCount(uid,startTime,endTime);
+		DataManagementResult dataManagementResult = new DataManagementResult();
+		dataManagementResult.setCode(0);
+		dataManagementResult.setMsg("");
+		dataManagementResult.setCount(count);
+		dataManagementResult.setData(list);
+		return dataManagementResult;
+	}
+
+	@RequestMapping("/cashierResign")
+	@ResponseBody
+	public DataManagementResult cashierResign(String page, String limit, String uid, String startTime, String endTime, String resignId)
+	{
+		int pageInt = Integer.valueOf(page);
+		int limitInt = Integer.valueOf(limit);
+		int resign = adminService.resignState(resignId);
+		List<TbCashier> list = adminService.findAll(uid,pageInt,limitInt,startTime,endTime);
+		int count = adminService.findCount(uid,startTime,endTime);
+		DataManagementResult dataManagementResult = new DataManagementResult();
+		dataManagementResult.setCode(0);
+		dataManagementResult.setMsg("");
+		dataManagementResult.setCount(count);
+		dataManagementResult.setData(list);
+		return dataManagementResult;
+	}
+
+	@RequestMapping("/cashierReset")
+	@ResponseBody
+	public DataManagementResult cashierReset(String page, String limit, String uid, String startTime, String endTime,String resetId)
+	{
+		int pageInt = Integer.valueOf(page);
+		int limitInt = Integer.valueOf(limit);
+		int rest = adminService.resetPwd(resetId);
+		List<TbCashier> list = adminService.findAll(uid,pageInt,limitInt,startTime,endTime);
+		int count = adminService.findCount(uid,startTime,endTime);
+		DataManagementResult dataManagementResult = new DataManagementResult();
+		dataManagementResult.setCode(0);
+		dataManagementResult.setMsg("");
+		dataManagementResult.setCount(count);
+		dataManagementResult.setData(list);
+		return dataManagementResult;
+	}
 
     @RequestMapping("/addCashier")
     @ResponseBody
--- src/main/webapp/WEB-INF/administration/jsp/admin/adminManagement.jsp
+++ src/main/webapp/WEB-INF/administration/jsp/admin/adminManagement.jsp
@@ -225,11 +225,17 @@
 			if(obj.event === 'forbidden'){
 				layer.confirm('确定要禁用?', {icon: 2, title:'提示'}, function(index) {
 				var table =layui.table, $=layui.jquery;
+				var uid = $("#uid").val();
+				var startTime = $("#startTime").val();
+				var endTime = $("#endTime").val();
 				var stateId = obj.data.cashierId;
 				table.reload('one',{
 					url: "${pageContext.request.contextPath}/admin/cashierForbidden",
 					method:'post',
 					where:{
+						'uid':uid,
+						'startTime':startTime,
+						'endTime':endTime,
 						'stateId':stateId
 					},
 					page:{
@@ -247,17 +253,14 @@
 				var startTime = $("#startTime").val();
 				var endTime = $("#endTime").val();
 				var oId = obj.data.cashierId;
-				var stateId = null;
-				var resignId = null;
 				table.reload('one',{
 					method:'post',
+					url: "${pageContext.request.contextPath}/admin/cashierOpen",
 					where:{
 						'uid':uid,
 						'startTime':startTime,
 						'endTime':endTime,
-						'oId':oId,
-						'stateId':stateId,
-						'resignId':resignId
+						'oId':oId
 					},
 					page:{
 						page:1
@@ -273,17 +276,14 @@
 					var uid = $("#uid").val();
 					var startTime = $("#startTime").val();
 					var endTime = $("#endTime").val();
-					var oId = null;
-					var stateId = null;
 					var resignId = obj.data.cashierId;
 					table.reload('one',{
 						method:'post',
+						url: "${pageContext.request.contextPath}/admin/cashierResign",
 						where:{
 							'uid':uid,
 							'startTime':startTime,
 							'endTime':endTime,
-							'oId':oId,
-							'stateId':stateId,
 							'resignId':resignId
 						},
 						page:{
@@ -300,19 +300,14 @@
 					var uid = $("#uid").val();
 					var startTime = $("#startTime").val();
 					var endTime = $("#endTime").val();
-					var oId = null;
-					var stateId = null;
-					var resignId = null;
 					var resetId = obj.data.cashierId;
 					table.reload('one',{
 						method:'post',
+						url: "${pageContext.request.contextPath}/admin/cashierReset",
 						where:{
 							'uid':uid,
 							'startTime':startTime,
 							'endTime':endTime,
-							'oId':oId,
-							'stateId':stateId,
-							'resignId':resignId,
 							'resetId':resetId
 						},
 						page:{
